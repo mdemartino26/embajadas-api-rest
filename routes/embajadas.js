fc9613d741ec6b6058783f0e3261f494dfc62311
mdemartino26 <55245840+mdemartino26@users.noreply.github.com>
2020-06-06 23:41:05 -0300

    routes

--- routes/embajadas.js
+++ routes/embajadas.js
@@ -12,8 +12,65 @@ const crudEmbajadas = (app) => {
         })
     }
 
+    //POST - Insert a new register in the DB
+    addEmbajada = function (req, res) {
+        console.log('POST');
+        console.log(req.body);
+        var embajada = new Embajada({
+            lat: req.body.lat,
+            lng: req.body.lng,
+            name: req.body.name,
+            description: req.body.description,
+            type: req.body.type
+        });
+        embajada.save(function (err) {
+            if (!err) {
+                console.log('Created',req.body);
+            } else {
+                console.log('ERROR: ' + err);
+            }
+        });
+        res.send(embajada);
+    };
+
+    //PUT - Update a register already exists in the DB
+    modificarEmbajada = function (req, res) {
+        Embajada.findById(req.params.id, function (err, embajada) {
+            embajada.lat = req.body.lat;
+            embajada.lng = req.body.lng;
+            embajada.name = req.body.name;
+            embajada.description = req.body.description;
+            embajada.type = req.body.type;
+            embajada.save(function (err) {
+                if (!err) {
+                    console.log('Updated',req.body);
+                } else {
+                    console.log('ERROR: ' + err);
+                }
+                res.send(embajada);
+            });
+        });
+    }
+
+    //DELETE - Delete a register with specified ID
+    deleteEmbajada = function (req, res) {
+        Embajada.findById(req.params.id, function (err, embajada) {
+            embajada.remove(function (err) {
+                if (!err) {
+                    console.log('Removed',req.body);
+                } else {
+                    console.log('ERROR: ' + err);
+                }
+                res.send(embajada);
+            })
+        });
+    }
+
     //URLS
     app.get('/markers', findAllEmbajadas);
+    app.post('/markers', addEmbajada);
+    app.put('/markers/:id', modificarEmbajada);
+    app.delete('/markers/:id', deleteEmbajada);
 }
 
     module.exports = crudEmbajadas;
